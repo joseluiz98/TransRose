303392ccdf83a3797f211afba5ef2a0f38169381
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2017-10-29 19:42:27 -0200

    v3.0 Iniciando implementação de API Google Drive Todos os dados irão para nuvem em breve

--- TransRose/Program.cs
+++ TransRose/Program.cs
@@ -1,16 +1,170 @@
-﻿namespace WindowsFormsApplication2
-{
-    using System;
-    using System.Windows.Forms;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-    public class Program
+namespace GoogleDriveAPIDemo
+{
+    class Program
     {
-        [STAThread]
-        private static void Main()
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "cafeasp demo 01";
+
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new VerCadastros());
+            UserCredential credential;
+
+            credential = GetCredentials();
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+
+            //UploadBasicImage("you file full path", service);
+
+            string pageToken = null;
+
+            do
+            {
+                ListFiles(service, ref pageToken);
+
+            } while (pageToken != null);
+
+            Console.WriteLine("Done");
+            Console.Read();
+
+
+        }
+
+        private static void ListFiles(DriveService service, ref string pageToken)
+        {
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 1;
+            //listRequest.Fields = "nextPageToken, files(id, name)";
+            listRequest.Fields = "nextPageToken, files(name)";
+            listRequest.PageToken = pageToken;
+            listRequest.Q = "mimeType='application/msaccess'";
+
+            // List files.
+            var request = listRequest.Execute();
+
+
+            if (request.Files != null && request.Files.Count > 0)
+            {
+
+
+                foreach (var file in request.Files)
+                {
+                    Console.WriteLine("File ID: " + file.Id);
+
+                    var fileId = file.Id;
+                    var Request = service.Files.Get(fileId);
+                    var stream = new System.IO.MemoryStream();
+
+                    // Add a handler which will be notified on progress changes.
+                    // It will notify on each chunk download and when the
+                    // download is completed or failed.
+                    Request.MediaDownloader.ProgressChanged +=
+                        (IDownloadProgress progress) =>
+                        {
+                            switch (progress.Status)
+                            {
+                                case DownloadStatus.Downloading:
+                                    {
+                                        Console.WriteLine(progress.BytesDownloaded);
+                                        break;
+                                    }
+                                case DownloadStatus.Completed:
+                                    {
+                                        Console.WriteLine("Download complete.");
+                                        break;
+                                    }
+                                case DownloadStatus.Failed:
+                                    {
+                                        Console.WriteLine("Download failed.");
+                                        break;
+                                    }
+                            }
+                        };
+                    Request.Download(stream);
+                    //MessageBox.Show(file.Name, "", MessageBoxButtons.OK);
+                }
+
+                pageToken = request.NextPageToken;
+
+                if (request.NextPageToken != null)
+                {
+                    Console.WriteLine("Press any key to conti...");
+                    Console.ReadLine();
+
+
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+
+            }
+
+
+        }
+
+        private static void UploadBasicImage(string path, DriveService service)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File();
+            fileMetadata.Name = Path.GetFileName(path);
+            fileMetadata.MimeType = "image/jpeg";
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+            {
+                request = service.Files.Create(fileMetadata, stream, "image/jpeg");
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            var file = request.ResponseBody;
+
+            Console.WriteLine("File ID: " + file.Id);
+
+        }
+
+        private static UserCredential GetCredentials()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                // Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            return credential;
         }
     }
 }
